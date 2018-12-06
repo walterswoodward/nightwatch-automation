ffb75f7efa234bd046380a601a9e69ded2ae899e
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-12-06 15:20:08 -0500

    myfirsttest.js commented out

--- tests/myfirsttest.js
+++ tests/myfirsttest.js
@@ -1,14 +1,34 @@
-module.exports = {
-  // browser given as default input always
-  "Demo Test Google": function(browser) {
-    browser
-      .url("http://google.com")
-      // 1000 - time to wait for the el to be visible
-      .waitForElementVisible("body", 1000)
-      .setValue("input[type=text]", "nightwatch")
-      .waitForElementVisible("input[name=btnK]", 1000)
-      .click("input[name=btnK]")
-      .pause(1000)
-      .end();
-  }
-};
+// module.exports = {
+  // // browser given as default input always
+  // "Demo Test Google": function(browser) {
+  //   browser
+  //     .url("http://google.com")
+  //     // 1000 - time to wait for the el to be visible
+  //     .waitForElementVisible("body", 1000)
+  //     .setValue("input[type=text]", "nightwatch")
+  //     .waitForElementVisible("input[name=btnK]", 1000)
+  //     .click("input[name=btnK]")
+  //     .pause(1000)
+  //     .end();
+  // },
+  // // browser given as default input always
+  // "Demo Test Amazon": function(browser) {
+  //   browser
+  //     .url("http://amazon.com")
+  //     // 1000 - time to wait for the el to be visible
+  //     .waitForElementVisible("body", 1000)
+  //     .setValue("input[type=text]", "nightwatch")
+  //     .waitForElementVisible("#nav-search > form > div.nav-right > div > input", 1000)
+  //     .click("#nav-search > form > div.nav-right > div > input")
+  //     .pause(1000)
+  //     .end();
+  // },
+
+  // "Confirm Amazon Logo Visible": function(browser) {
+  //   browser
+  //     .url("http://amazon.com")
+  //     browser.expect.element('[aria-label] .nav-logo-base').to.be.present;
+  //     // or assert the element is visible
+  //     browser.expect.element('[aria-label] .nav-logo-base').to.be.visible;
+  // }
+// };
