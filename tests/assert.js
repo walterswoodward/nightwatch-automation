38e95183b9167f76bed85bb3d5b31ef9a4d8e661
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-12-06 17:24:43 -0500

    adds more code experimenting with NW assert

--- tests/assert.js
+++ tests/assert.js
@@ -1,18 +1,41 @@
 module.exports = {
+  url: 'http://google.com',
   before: function(browser) {
-    console.log("Setting up...");
+    console.log("Setting up Assert Tests...");
   },
 
   after: function(browser) {
-    console.log("Closing down...");
+    console.log("Closing down Assert Tests...");
   },
-  "Demo Test Google": function(browser) {
-    browser
-    .url("http://google.com")
-    .assert.visible("html");
+  "@tags": ["demo", "height", "glink"],
+               
+  "Demo Test Google":
+  // This test is ignored by turning it into a string
+    "" +
+    function(browser) {
+      browser.url(url).assert.visible("html");
 
+      browser
+        .url(url)
+        // Switch to xPath Locator Syntax
+        .useXpath()
+        .verify.visible("//img")
+        .end();
+    },
+
+  "Test Google Logo Height": function(browser) {
+    // Switch from xPath back to using CSS locator
+    browser.url(url).useCss();
+    // Checks if the given attribute (href) of an element (a) contains the expected value (google.com).
+    browser.assert.attributeContains("img", "height", "92");
+    // Checks if the given attribute (class) of an element (textarea) has the expected value (csi).
+    browser.assert.attributeEquals("textarea", "class", "csi").end();
+  },
+
+  GLink: function(browser) {
     browser
-    .url("http://google.com")
-    .verify.visible("html");
+      .url(url)
+      .assert.containsText("a[class=gb_P]", "Gmail")
+      .end();
   }
 };
