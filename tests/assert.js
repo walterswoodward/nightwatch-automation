838b41f1b808714af92f96e5fb042cfadb5e9598
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-12-07 09:55:35 -0500

    visibility test modified verify.visible() commented out in lieu of assert.visible() which will stop testing if no page is visible

--- tests/assert.js
+++ tests/assert.js
@@ -6,28 +6,35 @@ module.exports = {
   after: function(browser) {
     console.log("Closing down Assert Tests...");
   },
-  "@tags": ["demo", "height", "glink"],
 
-  "Demo Test Google":
+  // TODO: Add @tags
+  // "@tags": ["demo", "height", "glink"],
+
+  "Test Visibility w/ assert.visible": function(browser) {
+    // In contrast to verify.visible(), assert.visible() will end all tests if it fails
+    browser
+      .url(browser.launch_url)
+      .assert.visible("html")
+      .end();
+
+    // Use this only if you want the tests to continue EVEN if the page is not visible:
+    // browser
+    //   .url(browser.launch_url)
+    //   // Switch to xPath Locator Syntax
+    //   .useXpath()
+    //   .verify.visible("//img")
+    //   .end();
+  },
+
+  "Test Logo Height w/ assert.attributeContains":
     // This test is ignored by turning it into a string
     "" +
     function(browser) {
-      browser.url(browser.launch_url).assert.visible("html");
-
-      browser
-        .url(browser.launch_url)
-        // Switch to xPath Locator Syntax
-        .useXpath()
-        .verify.visible("//img")
-        .end();
+      // Switch from xPath back to using CSS locator
+      browser.url(browser.launch_url).useCss();
+      // Checks if the given attribute (href) of an element (a) contains the expected value (google.com).
+      browser.assert.attributeContains("img", "height", "92");
     },
-
-  "Test Logo Height w/ assert.attributeContains": function(browser) {
-    // Switch from xPath back to using CSS locator
-    browser.url(browser.launch_url).useCss();
-    // Checks if the given attribute (href) of an element (a) contains the expected value (google.com).
-    browser.assert.attributeContains("img", "height", "92");
-  },
   "Test textarea class w/ assert.attributeEquals": function(browser) {
     browser.url(browser.launch_url);
     // Checks if the given attribute (class) of an element (textarea) has the expected value (csi).
